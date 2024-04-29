e9cacf58476aec8a883429eed841ce1883e9fb4e
Tan Ke Liang <KELIANG.23@ichat.sp.edu.sg>
2024-04-29 22:31:03 +0800

    new changes made

--- bmi.py
+++ bmi.py
@@ -1,11 +1,13 @@
-def calculate_bmi(height, weight):
-    print("Height="  + str(height))
-    print("Weight="  + str(weight))
+def calculate_bmi (height, weight):
+    print("Height = " + str(height))
+    print("Weigh = " + str(weight))
+    BMI = weight/(height*height)
+    print("BMI is " + str(BMI))
+    if BMI < 18.5:
+        print("Under Weight")
+    elif 18.5<=BMI<=25:
+        print("Normal Weight")
+    else:
+        print("Over Weight")
 
-    #Add code here to calculate BMI
-    bmi = float(weight) / (float(height) * float(height))
-
-    #Add code here to display calculate BMI
-    print("The calculated BMI is " + str(bmi) + ".")
-
-calculate_bmi(weight="57", height="1.73")
+calculate_bmi(weight = 57, height = 1.73)
