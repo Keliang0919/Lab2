3353306f94602690954d0eedc3bfdad43cc403de
Tan Ke Liang <KELIANG.23@ichat.sp.edu.sg>
2024-04-29 22:35:10 +0800

    Return value added

--- bmi.py
+++ bmi.py
@@ -5,9 +5,12 @@ def calculate_bmi (height, weight):
     print("BMI is " + str(BMI))
     if BMI < 18.5:
         print("Under Weight")
+        print("Return value: -1")
     elif 18.5<=BMI<=25:
         print("Normal Weight")
+        print("Return value: 0")
     else:
         print("Over Weight")
+        print("Return value: 1")
 
 calculate_bmi(weight = 57, height = 1.73)
